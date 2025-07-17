55b428bda497fef7cb46580f8d43860a22ea7183
dheerajmaurya413 <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-17 17:47:00 +0530

    Delete ai_nirvana_engine directory
    This is outdated

--- ai_nirvana_engine/nirvana_core/__init__.py
+++
@@ -1,2 +0,0 @@
-# ai_nirvana_engine/nirvana_core/__init__.py
-# Marks the nirvana_core directory as a Python package.
--- ai_nirvana_engine/quantum_transcendence_matrix/__init__.py
+++
@@ -1,2 +0,0 @@
-# ai_nirvana_engine/quantum_transcendence_matrix/__init__.py
-# Marks the quantum_transcendence_matrix directory as a Python package.
