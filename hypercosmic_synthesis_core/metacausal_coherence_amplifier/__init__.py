338f4c1b1cb96996bbef2194a4d14295c4259095
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:06:39 +0530

    Create __init__.py

---
+++ hypercosmic_synthesis_core/metacausal_coherence_amplifier/__init__.py
@@ -0,0 +1,3 @@
+# hypercosmic_synthesis_core/metacausal_coherence_amplifier/__init__.py
+# Marks the metacausal_coherence_amplifier directory as a Python package.
+__all__ = ['MetacausalCoherenceAmplifier']
