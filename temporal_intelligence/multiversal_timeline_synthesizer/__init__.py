b4e7d6015d1114a5eb10a2763c47e110924d97d6
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:42:20 +0530

    Create __init__.py

---
+++ temporal_intelligence/multiversal_timeline_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# temporal_intelligence/multiversal_timeline_synthesizer/__init__.py
+# Marks the multiversal_timeline_synthesizer directory as a Python package.
+__all__ = ['MultiversalTimelineSynthesizer']
