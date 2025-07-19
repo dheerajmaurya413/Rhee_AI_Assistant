56b4099aecc37fbc77fc1a4f179dfefd120b11d9
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:50:49 +0530

    Create __init__.py

---
+++ metainfinite_causality_engine/metainfinite_causality_lattice/__init__.py
@@ -0,0 +1,3 @@
+# metainfinite_causality_engine/metainfinite_causality_lattice/__init__.py
+# Marks the metainfinite_causality_lattice directory as a Python package.
+__all__ = ['MetainfiniteCausalityLattice']
