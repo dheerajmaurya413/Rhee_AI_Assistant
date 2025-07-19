49ae2ad63d2363306bc51ed003af540adf9e280d
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:42:43 +0530

    Create __init__.py

---
+++ omniversal_sentience_nexus/metatemporal_resonance_field/__init__.py
@@ -0,0 +1,3 @@
+# omniversal_sentience_nexus/metatemporal_resonance_field/__init__.py
+# Marks the metatemporal_resonance_field directory as a Python package.
+__all__ = ['MetatemporalResonanceField']
