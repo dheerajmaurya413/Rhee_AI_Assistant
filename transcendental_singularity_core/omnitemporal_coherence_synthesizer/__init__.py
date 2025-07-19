e2e868c2a791c39b2ebe82ebe74685bf619ef502
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:32:56 +0530

    Create __init__.py

---
+++ transcendental_singularity_core/omnitemporal_coherence_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# transcendental_singularity_core/omnitemporal_coherence_synthesizer/__init__.py
+# Marks the omnitemporal_coherence_synthesizer directory as a Python package.
+__all__ = ['OmnitemporalCoherenceS9Synthesizer']
