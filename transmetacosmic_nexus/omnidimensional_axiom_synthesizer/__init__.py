294e1262674e481d1be713d5e6ecfda96441aaa1
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:17:38 +0530

    Create __init__.py

---
+++ transmetacosmic_nexus/omnidimensional_axiom_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# transmetacosmic_nexus/omnidimensional_axiom_synthesizer/__init__.py
+# Marks the omnidimensional_axiom_synthesizer directory as a Python package.
+__all__ = ['OmnidimensionalAxiomSynthesizer']
