6e231743b14593060c8f8de962ad86e2719748cb
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:25:45 +0530

    Create __init__.py

---
+++ cosmic_intelligence_orchestrator/omniversal_coherence_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# cosmic_intelligence_orchestrator/omniversal_coherence_synthesizer/__init__.py
+# Marks the omniversal_coherence_synthesizer directory as a Python package.
+__all__ = ['OmniversalCoherenceSynthesizer']
