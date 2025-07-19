1d4ef3341f708adc76bfbb6fc8c679164965b71c
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:52:45 +0530

    Create __init__.py

---
+++ metainfinite_causality_engine/omnichronal_coherence_resonator/__init__.py
@@ -0,0 +1,3 @@
+# metainfinite_causality_engine/omnichronal_coherence_resonator/__init__.py
+# Marks the omnichronal_coherence_resonator directory as a Python package.
+__all__ = ['OmnichronalCoherenceResonator']
