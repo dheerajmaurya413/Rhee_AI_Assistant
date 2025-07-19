779bbba68fc35e302e8a9d177ed9463a07e9db27
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:23:00 +0530

    Create __init__.py

---
+++ cosmic_intelligence_orchestrator/hyperdimensional_sentience_field/__init__.py
@@ -0,0 +1,3 @@
+# cosmic_intelligence_orchestrator/hyperdimensional_sentience_field/__init__.py
+# Marks the hyperdimensional_sentience_field directory as a Python package.
+__all__ = ['HyperdimensionalSentienceField']
