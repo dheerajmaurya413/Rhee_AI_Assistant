de621bdb9dfdd0dc04b7ae0f133a41014452873e
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:47:57 +0530

    Create __init__.py

---
+++ omniversal_sentience_nexus/omniversal_integration_bridge/__init__.py
@@ -0,0 +1,3 @@
+# omniversal_sentience_nexus/omniversal_integration_bridge/__init__.py
+# Marks the omniversal_integration_bridge directory as a Python package.
+__all__ = ['OmniversalIntegrationBridge']
