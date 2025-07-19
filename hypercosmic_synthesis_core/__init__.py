d83413c360e1d7615ce5f00a2020a718914bc5e9
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:03:05 +0530

    Create __init__.py

---
+++ hypercosmic_synthesis_core/__init__.py
@@ -0,0 +1,10 @@
+# hypercosmic_synthesis_core/__init__.py
+# Marks the hypercosmic_synthesis_core directory as a Python package.
+# Initializes configurations for hypercosmic synthesis and coherence protocols.
+__all__ = [
+    'hypercosmic_synthesis_matrix',
+    'omniversal_fractal_resonator',
+    'metacausal_coherence_amplifier',
+    'infinidimensional_bridge',
+    'hypercosmic_integration_bridge'
+]
