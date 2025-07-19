ec4e366baf8503d9cf98b66c0a8c6d81c2697107
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:34:24 +0530

    Create __init__.py

---
+++ transcendental_singularity_core/infiniversal_axiom_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# transcendental_singularity_core/infiniversal_axiom_orchestrator/__init__.py
+# Marks the infniversal_axiom_orchestrator directory as a Python package.
+__all__ = ['InfiniversalAxiomOrchestrator']
