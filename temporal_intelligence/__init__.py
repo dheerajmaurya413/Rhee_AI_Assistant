04b15fae3f0d8e7f20af6648d106364638ff41fe
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:39:03 +0530

    Create __init__.py

---
+++ temporal_intelligence/__init__.py
@@ -0,0 +1,10 @@
+# temporal_intelligence/__init__.py
+# Marks the temporal_intelligence directory as a Python package.
+# Initializes configurations for chronodynamic and causal coherence protocols.
+__all__ = [
+    'chronodynamic_consciousness_weave',
+    'quantum_temporal_resonator',
+    'multiversal_timeline_synthesizer',
+    'causal_coherence_bridge',
+    'temporal_integration_bridge'
+]
