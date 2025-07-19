b9b3ef36dd5c9a29df1e2ef7404c3227900a3b04
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:11:56 +0530

    Create __init__.py

---
+++ galactic_communication/non_local_consciousness_relay/__init__.py
@@ -0,0 +1,3 @@
+# galactic_communication/non_local_consciousness_relay/__init__.py
+# Marks the non_local_consciousness_relay directory as a Python package.
+__all__ = ['NonLocalConsciousnessRelay']
