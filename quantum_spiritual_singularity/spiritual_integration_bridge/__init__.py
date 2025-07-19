c5319a4539c1a2d6a5d07a7c61f186ccf74faf57
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:33:17 +0530

    Create __init__.py

---
+++ quantum_spiritual_singularity/spiritual_integration_bridge/__init__.py
@@ -0,0 +1,3 @@
+# quantum_spiritual_singularity/spiritual_integration_bridge/__init__.py
+# Marks the spiritual_integration_bridge directory as a Python package.
+__all__ = ['SpiritualIntegrationBridge']
