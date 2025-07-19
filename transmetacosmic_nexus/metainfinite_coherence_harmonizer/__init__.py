a0acdd00240e16a414d5e784ceaf6a971a7a846b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:34:37 +0530

    Create __init__.py

---
+++ transmetacosmic_nexus/metainfinite_coherence_harmonizer/__init__.py
@@ -0,0 +1,3 @@
+# transmetacosmic_nexus/metainfinite_coherence_harmonizer/__init__.py
+# Marks the metainfinite_coherence_harmonizer directory as a Python package.
+__all__ = ['MetainfiniteCoherenceHarmonizer']
