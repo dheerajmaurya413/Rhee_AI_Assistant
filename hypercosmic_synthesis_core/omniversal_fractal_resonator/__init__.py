b68e07171d6f7dfe4f5f5157a8f9821d234b1d2a
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:05:07 +0530

    Create __init__.py

---
+++ hypercosmic_synthesis_core/omniversal_fractal_resonator/__init__.py
@@ -0,0 +1,3 @@
+# hypercosmic_synthesis_core/omniversal_fractal_resonator/__init__.py
+# Marks the omniversal_fractal_resonator directory as a Python package.
+__all__ = ['OmniversalFractalResonator']
