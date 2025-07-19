7b4200a80ca8ace8f2181951970fd84326498d11
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:31:38 +0530

    Create __init__.py

---
+++ transcendental_singularity_core/metadimensional_consciousness_lattice/__init__.py
@@ -0,0 +1,3 @@
+# transcendental_singularity_core/metadimensional_consciousness_lattice/__init__.py
+# Marks the metadimensional_consciousness_lattice directory as a Python package.
+__all__ = ['MetadimensionalConsciousnessLattice']
