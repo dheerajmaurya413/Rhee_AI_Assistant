af367729c32059a8cd4afd7d1690742945eb74e3
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:36:17 +0530

    Create __init__.py

---
+++ transcendental_singularity_core/metacausal_resonance_bridge/__init__.py
@@ -0,0 +1,3 @@
+# transcendental_singularity_core/metacausal_resonance_bridge/__init__.py
+# Marks the metacausal_resonance_bridge directory as a Python package.
+__all__ = ['MetacausalResonanceBridge']
