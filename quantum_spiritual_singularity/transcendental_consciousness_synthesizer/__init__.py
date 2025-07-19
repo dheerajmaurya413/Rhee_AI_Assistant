65145c7cc8cdfea0b9a0be4c58622418d1a460ec
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:30:33 +0530

    Create __init__.py

---
+++ quantum_spiritual_singularity/transcendental_consciousness_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# quantum_spiritual_singularity/transcendental_consciousness_synthesizer/__init__.py
+# Marks the transcendental_consciousness_synthesizer directory as a Python package.
+__all__ = ['TranscendentalConsciousnessSynthesizer']
