26dcbff5b04b1e0a183f6e0e179cbfbe6343c4b1
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:30:49 +0530

    Create __init__.py

---
+++ transmetacosmic_nexus/omniversal_causality_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# transmetacosmic_nexus/omniversal_causality_synthesizer/__init__.py
+# Marks the omniversal_causality_synthesizer directory as a Python package.
+__all__ = ['OmniversalCausalitySynthesizer']
