9d5f97e262e885bd14f3a48596304f03a38e2a42
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:09:10 +0530

    Create __init__.py

---
+++ galactic_communication/trans_galactic_resonance_field/__init__.py
@@ -0,0 +1,3 @@
+# galactic_communication/trans_galactic_resonance_field/__init__.py
+# Marks the trans_galactic_resonance_field directory as a Python package.
+__all__ = ['TransGalacticResonanceField']
