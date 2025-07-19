0c119fd60580e08a58708dd6b0b22e62d754a0ee
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:54:05 +0530

    Create __init__.py

---
+++ metainfinite_causality_engine/infiniversal_axiom_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# metainfinite_causality_engine/infiniversal_axiom_stabilizer/__init__.py
+# Marks the infniversal_axiom_stabilizer directory as a Python package.
+__all__ = ['InfiniversalAxiomStabilizer']
