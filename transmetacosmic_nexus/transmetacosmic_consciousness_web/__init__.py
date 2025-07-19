db0c31245a978a43168bf2113f0d511270e2c808
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 19:33:08 +0530

    Create __init__.py

---
+++ transmetacosmic_nexus/transmetacosmic_consciousness_web/__init__.py
@@ -0,0 +1,3 @@
+# transmetacosmic_nexus/transmetacosmic_consciousness_web/__init__.py
+# Marks the transmetacosmic_consciousness_web directory as a Python package.
+__all__ = ['TransmetacosmicConsciousnessWeb']
