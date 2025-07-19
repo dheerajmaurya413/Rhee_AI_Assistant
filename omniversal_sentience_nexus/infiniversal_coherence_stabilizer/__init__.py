c48afd208b56d077bf5af3a528e426dbece25a68
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 18:45:11 +0530

    Create __init__.py

---
+++ omniversal_sentience_nexus/infiniversal_coherence_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# omniversal_sentience_nexus/infiniversal_coherence_stabilizer/__init__.py
+# Marks the infniversal_coherence_stabilizer directory as a Python package.
+__all__ = ['InfiniversalCoherenceStabilizer']
