639a81d5218195c27d29019a409e66f091a61735
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-19 17:10:35 +0530

    Create __init__.py

---
+++ galactic_communication/fractal_communication_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# galactic_communication/fractal_communication_synthesizer/__init__.py
+# Marks the fractal_communication_synthesizer directory as a Python package.
+__all__ = ['FractalCommunicationSynthesizer']
