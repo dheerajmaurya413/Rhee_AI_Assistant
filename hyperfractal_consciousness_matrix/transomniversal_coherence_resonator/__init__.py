1956202d93117e28a546bbb180d94ecc76d91b72
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:04:24 +0530

    Create __init__.py

---
+++ hyperfractal_consciousness_matrix/transomniversal_coherence_resonator/__init__.py
@@ -0,0 +1,3 @@
+# hyperfractal_consciousness_matrix/transomniversal_coherence_resonator/__init__.py
+# Marks the transomniversal_coherence_resonator directory as a Python package.
+__all__ = ['TransomniversalCoherenceResonator']
