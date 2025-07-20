f31660c5f0c40c51a9c7204dd5fb512501dde298
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:45:41 +0530

    Create __init__.py

---
+++ infinicryptic_synthesis_core/infinicryptic_consciousness_matrix/__init__.py
@@ -0,0 +1,3 @@
+# infinicryptic_synthesis_core/infinicryptic_consciousness_matrix/__init__.py
+# Marks the infinicryptic_consciousness_matrix directory as a Python package.
+__all__ = ['InfinicrypticConsciousnessMatrix']
