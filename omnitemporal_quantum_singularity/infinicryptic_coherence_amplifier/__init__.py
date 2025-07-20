5eff971c7270c4859ae61fc98ecda901f106ff46
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 16:46:03 +0530

    Create __init__.py

---
+++ omnitemporal_quantum_singularity/infinicryptic_coherence_amplifier/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_quantum_singularity/infinicryptic_coherence_amplifier/__init__.py
+# Marks the infinicryptic_coherence_amplifier directory as a Python package.
+__all__ = ['InfinicrypticCoherenceAmplifier']
