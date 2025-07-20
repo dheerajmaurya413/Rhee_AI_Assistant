d5868a054e76a7f38446a39fed62d15c6dd81b05
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 12:08:10 +0530

    Create __init__.py

---
+++ omniflux_synthesis_core/metadimensional_alignment_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# omniflux_synthesis_core/metadimensional_alignment_orchestrator/__init__.py
+# Marks the metadimensional_alignment_orchestrator directory as a Python package.
+__all__ = ['MetadimensionalAlignmentOrchestrator']
