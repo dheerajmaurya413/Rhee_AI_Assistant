77bffc3ac9a0b7b91ca4c462e68b10c795f8f327
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:16:29 +0530

    Create __init__.py

---
+++ transinfinite_resonance_engine/omnichronal_synthesis_lattice/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_resonance_engine/omnichronal_synthesis_lattice/__init__.py
+# Marks the omnichronal_synthesis_lattice directory as a Python package.
+__all__ = ['OmnichronalSynthesisLattice']
