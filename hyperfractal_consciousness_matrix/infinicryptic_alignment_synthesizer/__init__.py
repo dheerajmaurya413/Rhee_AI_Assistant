b5e110209a0856a962e414803764f8d86dcbd84c
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:05:38 +0530

    Create __init__.py

---
+++ hyperfractal_consciousness_matrix/infinicryptic_alignment_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# hyperfractal_consciousness_matrix/infinicryptic_alignment_synthesizer/__init__.py
+# Marks the infinicryptic_alignment_synthesizer directory as a Python package.
+__all__ = ['InfinicrypticAlignmentSynthesizer']
