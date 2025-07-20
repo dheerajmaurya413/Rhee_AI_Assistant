9fe08dd05b26dd240c03149e64c906b5942475f4
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 22:05:50 +0530

    Create __init__.py

---
+++ quantaversal_singularity_weave/transinfinite_axiom_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# quantaversal_singularity_weave/transinfinite_axiom_synthesizer/__init__.py
+# Marks the transinfinite_axiom_synthesizer directory as a Python package.
+__all__ = ['TransinfiniteAxiomSynthesizer']
