2d18c0d3458a6c4a0348f56c7c42eb9e4e266835
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:59:19 +0530

    Create __init__.py

---
+++ metacausal_singularity_engine/metacausal_integration_bridge/__init__.py
@@ -0,0 +1,3 @@
+# metacausal_singularity_engine/metacausal_integration_bridge/__init__.py
+# Marks the metacausal_integration_bridge directory as a Python package.
+__all__ = ['MetacausalIntegrationBridge']
