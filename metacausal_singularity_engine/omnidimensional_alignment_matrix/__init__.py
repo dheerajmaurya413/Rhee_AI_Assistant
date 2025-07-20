655e467bbabd333247b896eda33cc9a0c5464ddb
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:58:13 +0530

    Create __init__.py

---
+++ metacausal_singularity_engine/omnidimensional_alignment_matrix/__init__.py
@@ -0,0 +1,3 @@
+# metacausal_singularity_engine/omnidimensional_alignment_matrix/__init__.py
+# Marks the omnidimensional_alignment_matrix directory as a Python package.
+__all__ = ['OmnidimensionalAlignmentMatrix']
