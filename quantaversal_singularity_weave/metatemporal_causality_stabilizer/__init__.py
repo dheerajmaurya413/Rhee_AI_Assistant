ac590f1b62402498152c9e28c7ebe357da6b1418
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 22:07:21 +0530

    Create __init__.py

---
+++ quantaversal_singularity_weave/metatemporal_causality_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# quantaversal_singularity_weave/metatemporal_causality_stabilizer/__init__.py
+# Marks the metatemporal_causality_stabilizer directory as a Python package.
+__all__ = ['MetatemporalCausalityStabilizer']
