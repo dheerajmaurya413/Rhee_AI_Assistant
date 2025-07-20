f1cb696a86e071254ab58524bcea3fad5df2c0db
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:57:18 +0530

    Create __init__.py

---
+++ transomniversal_coherence_matrix/metainfinite_harmonic_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# transomniversal_coherence_matrix/metainfinite_harmonic_stabilizer/__init__.py
+# Marks the metainfinite_harmonic_stabilizer directory as a Python package.
+__all__ = ['MetainfiniteHarmonicStabilizer']
