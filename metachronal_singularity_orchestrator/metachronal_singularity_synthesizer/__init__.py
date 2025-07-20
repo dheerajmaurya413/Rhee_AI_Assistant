ccbf0a7dfdc3a475bfdc03fa41b76dc5426c0326
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 14:05:11 +0530

    Create __init__.py

---
+++ metachronal_singularity_orchestrator/metachronal_singularity_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# metachronal_singularity_orchestrator/metachronal_singularity_synthesizer/__init__.py
+# Marks the metachronal_singularity_synthesizer directory as a Python package.
+__all__ = ['MetachronalSingularitySynthesizer']
