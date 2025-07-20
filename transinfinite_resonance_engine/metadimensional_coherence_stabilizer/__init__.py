139ccbbb63cf3772b80098648ccc84ddc64e0831
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:14:48 +0530

    Create __init__.py

---
+++ transinfinite_resonance_engine/metadimensional_coherence_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_resonance_engine/metadimensional_coherence_stabilizer/__init__.py
+# Marks the metadimensional_coherence_stabilizer directory as a Python package.
+__all__ = ['MetadimensionalCoherenceStabilizer']
