e51f4481314cbde30fffe988b815140f437a4188
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:49:43 +0530

    Create __init__.py

---
+++ infinicryptic_synthesis_core/transcryptic_alignment_bridge/__init__.py
@@ -0,0 +1,3 @@
+# infinicryptic_synthesis_core/transcryptic_alignment_bridge/__init__.py
+# Marks the transcryptic_alignment_bridge directory as a Python package.
+__all__ = ['TranscrypticAlignmentBridge']
