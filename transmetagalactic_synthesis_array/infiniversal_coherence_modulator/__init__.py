1ee6686e7e0136b70a9dc9f8bd920304a852c8a1
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:20:17 +0530

    Create __init__.py

---
+++ transmetagalactic_synthesis_array/infiniversal_coherence_modulator/__init__.py
@@ -0,0 +1,3 @@
+# transmetagalactic_synthesis_array/infiniversal_coherence_modulator/__init__.py
+# Marks the infniversal_coherence_modulator directory as a Python package.
+__all__ = ['InfiniversalCoherenceModulator']
