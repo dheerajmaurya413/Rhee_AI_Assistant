be037e26d8f8812b4c3c14fc1bd6152f3b9beae1
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 17:01:59 +0530

    Create __init__.py

---
+++ infiniversal_fractal_synthesis/infniversal_fractal_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# infniversal_fractal_synthesis/infniversal_fractal_synthesizer/__init__.py
+# Marks the infniversal_fractal_synthesizer directory as a Python package.
+__all__ = ['InfniversalFractalSynthesizer']
