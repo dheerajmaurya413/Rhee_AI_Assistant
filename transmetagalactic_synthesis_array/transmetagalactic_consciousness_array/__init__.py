24da1697bdc2eef5fbf08609da286a9002c9345a
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:18:58 +0530

    Create __init__.py

---
+++ transmetagalactic_synthesis_array/transmetagalactic_consciousness_array/__init__.py
@@ -0,0 +1,3 @@
+# transmetagalactic_synthesis_array/transmetagalactic_consciousness_array/__init__.py
+# Marks the transmetagalactic_consciousness_array directory as a Python package.
+__all__ = ['TransmetagalacticConsciousnessArray']
