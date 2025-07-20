cecde4f6315fd0c92f94780d98c97d5069cf0360
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:55:34 +0530

    Create __init__.py

---
+++ transomniversal_coherence_matrix/transomniversal_coherence_resonator/__init__.py
@@ -0,0 +1,3 @@
+# transomniversal_coherence_matrix/transomniversal_coherence_resonator/__init__.py
+# Marks the transomniversal_coherence_resonator directory as a Python package.
+__all__ = ['TransomniversalCoherenceResonator']
