d61e267dd006577f941c41d530333a70c9383667
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 14:14:46 +0530

    Create __init__.py

---
+++ metachronal_singularity_orchestrator/metachronal_integration_nexus/__init__.py
@@ -0,0 +1,3 @@
+# metachronal_singularity_orchestrator/metachronal_integration_nexus/__init__.py
+# Marks the metachronal_integration_nexus directory as a Python package.
+__all__ = ['MetachronalIntegrationNexus']
