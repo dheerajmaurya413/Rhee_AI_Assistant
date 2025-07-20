283829d2dac92e38184cdacd1f4ce5d76f3b9f21
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:18:08 +0530

    Create __init__.py

---
+++ transinfinite_resonance_engine/transinfinite_resonance_field/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_resonance_engine/transinfinite_resonance_field/__init__.py
+# Marks the transinfinite_resonance_field directory as a Python package.
+__all__ = ['TransinfiniteResonanceField']
