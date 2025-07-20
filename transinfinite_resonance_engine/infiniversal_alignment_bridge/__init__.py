7201dff6fb714f1ac724c280f5029e20bb34b2a3
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:13:29 +0530

    Create __init__.py

---
+++ transinfinite_resonance_engine/infiniversal_alignment_bridge/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_resonance_engine/infiniversal_alignment_bridge/__init__.py
+# Marks the infniversal_alignment_bridge directory as a Python package.
+__all__ = ['InfiniversalAlignmentBridge']
