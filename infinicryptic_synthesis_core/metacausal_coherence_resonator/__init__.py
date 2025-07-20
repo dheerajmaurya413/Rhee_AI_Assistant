31b02ad65b7bd409c3ebc6f3b8f219fca72f8d4b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:48:25 +0530

    Create __init__.py

---
+++ infinicryptic_synthesis_core/metacausal_coherence_resonator/__init__.py
@@ -0,0 +1,3 @@
+# infinicryptic_synthesis_core/metacausal_coherence_resonator/__init__.py
+# Marks the metacausal_coherence_resonator directory as a Python package.
+__all__ = ['MetacausalCoherenceResonator']
