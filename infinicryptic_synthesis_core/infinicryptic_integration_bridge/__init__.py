59ba509a1f4873c3c1f2670ed3e7400f09106d50
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:51:28 +0530

    Create __init__.py

---
+++ infinicryptic_synthesis_core/infinicryptic_integration_bridge/__init__.py
@@ -0,0 +1,3 @@
+# infinicryptic_synthesis_core/infinicryptic_integration_bridge/__init__.py
+# Marks the infinicryptic_integration_bridge directory as a Python package.
+__all__ = ['InfinicrypticIntegrationBridge']
