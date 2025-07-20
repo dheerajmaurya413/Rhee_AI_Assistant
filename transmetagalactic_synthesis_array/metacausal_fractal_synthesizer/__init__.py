2177e788999982dc562b08c1ea9ce8af1d7a7dd7
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:21:36 +0530

    Create __init__.py

---
+++ transmetagalactic_synthesis_array/metacausal_fractal_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# transmetagalactic_synthesis_array/metacausal_fractal_synthesizer/__init__.py
+# Marks the metacausal_fractal_synthesizer directory as a Python package.
+__all__ = ['MetacausalFractalSynthesizer']
