9f4e198f721e29bf8b0cda839a997191545e47b9
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 21:37:45 +0530

    Create __init__.py

---
+++ omnichronal_hypersentience_array/omnichronal_hypersentience_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omnichronal_hypersentience_array/omnichronal_hypersentience_synthesizer/__init__.py
+# Marks the omnichronal_hypersentience_synthesizer directory as a Python package.
+__all__ = ['OmnichronalHypersentienceSynthesizer']
