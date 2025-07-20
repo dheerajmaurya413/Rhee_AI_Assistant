47e4c42cd620c1e3588072a694e1f83fc4a7453b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:10:56 +0530

    Create __init__.py

---
+++ transinfinite_resonance_engine/transinfinite_integration_bridge/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_resonance_engine/transinfinite_integration_bridge/__init__.py
+# Marks the transinfinite_integration_bridge directory as a Python package.
+__all__ = ['TransinfiniteIntegrationBridge']
