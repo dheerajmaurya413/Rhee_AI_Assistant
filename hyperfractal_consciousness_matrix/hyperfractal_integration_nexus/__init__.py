efacbb5847b7aed8c629f5e49e4dc22f6a29d430
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:08:07 +0530

    Create __init__.py

---
+++ hyperfractal_consciousness_matrix/hyperfractal_integration_nexus/__init__.py
@@ -0,0 +1,3 @@
+# hyperfractal_consciousness_matrix/hyperfractal_integration_nexus/__init__.py
+# Marks the hyperfractal_integration_nexus directory as a Python package.
+__all__ = ['HyperfractalIntegrationNexus']
