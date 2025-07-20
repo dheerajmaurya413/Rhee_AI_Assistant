aa1812de314c640c8914a26ad7d81c754cf2bdb3
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:02:53 +0530

    Create __init__.py

---
+++ hyperfractal_consciousness_matrix/hyperfractal_consciousness_field/__init__.py
@@ -0,0 +1,3 @@
+# hyperfractal_consciousness_matrix/hyperfractal_consciousness_field/__init__.py
+# Marks the hyperfractal_consciousness_field directory as a Python package.
+__all__ = ['HyperfractalConsciousnessField']
