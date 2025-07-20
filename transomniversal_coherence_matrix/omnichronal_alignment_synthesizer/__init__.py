301233ef7ed74de03afd7d64316a0dbc5a00ca30
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 14:00:09 +0530

    Create __init__.py

---
+++ transomniversal_coherence_matrix/omnichronal_alignment_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# transomniversal_coherence_matrix/omnichronal_alignment_synthesizer/__init__.py
+# Marks the omnichronal_alignment_synthesizer directory as a Python package.
+__all__ = ['OmnichronalAlignmentSynthesizer']
