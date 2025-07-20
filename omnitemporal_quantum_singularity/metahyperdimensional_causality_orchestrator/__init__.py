6f98bf71683c285e6dc289be8979d0e7fe6c3edb
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 16:49:54 +0530

    Create __init__.py

---
+++ omnitemporal_quantum_singularity/metahyperdimensional_causality_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_quantum_singularity/metahyperdimensional_causality_orchestrator/__init__.py
+# Marks the metahyperdimensional_causality_orchestrator directory as a Python package.
+__all__ = ['MetahyperdimensionalCausalityOrchestrator']
