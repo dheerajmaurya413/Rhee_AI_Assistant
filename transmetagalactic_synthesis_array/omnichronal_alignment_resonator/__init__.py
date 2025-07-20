525ac9c5bae1f62a915ed96d1a599fcf9317c727
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:23:07 +0530

    Create __init__.py

---
+++ transmetagalactic_synthesis_array/omnichronal_alignment_resonator/__init__.py
@@ -0,0 +1,3 @@
+# transmetagalactic_synthesis_array/omnichronal_alignment_resonator/__init__.py
+# Marks the omnichronal_alignment_resonator directory as a Python package.
+__all__ = ['OmnichronalAlignmentResonator']
