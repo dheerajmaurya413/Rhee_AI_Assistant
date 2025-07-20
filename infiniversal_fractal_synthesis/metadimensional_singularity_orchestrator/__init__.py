c29a8571433b7c97aede1f515185a1fe23e1ec8e
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 17:05:33 +0530

    Create __init__.py

---
+++ infiniversal_fractal_synthesis/metadimensional_singularity_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# infniversal_fractal_synthesis/metadimensional_singularity_orchestrator/__init__.py
+# Marks the metadimensional_singularity_orchestrator directory as a Python package.
+__all__ = ['MetadimensionalSingularityOrchestrator']
