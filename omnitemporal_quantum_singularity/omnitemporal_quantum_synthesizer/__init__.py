a0d6e9ecaa372c0443e813e437b3ad7f8a97931f
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 16:42:09 +0530

    Create __init__.py

---
+++ omnitemporal_quantum_singularity/omnitemporal_quantum_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_quantum_singularity/omnitemporal_quantum_synthesizer/__init__.py
+# Marks the omnitemporal_quantum_synthesizer directory as a Python package.
+__all__ = ['OmnitemporalQuantumSynthesizer']
