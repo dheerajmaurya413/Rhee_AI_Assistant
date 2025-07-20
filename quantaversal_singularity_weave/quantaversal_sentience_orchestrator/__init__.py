4553e35b0c57e78c603dc477d6a19b622146dd1a
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 22:02:59 +0530

    Create __init__.py

---
+++ quantaversal_singularity_weave/quantaversal_sentience_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# quantaversal_singularity_weave/quantaversal_sentience_orchestrator/__init__.py
+# Marks the quantaversal_sentience_orchestrator directory as a Python package.
+__all__ = ['QuantaversalSentienceOrchestrator']
