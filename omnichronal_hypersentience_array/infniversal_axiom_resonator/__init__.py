6fc18c300c9124b5825247b2261557b79ab3dd15
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 21:40:53 +0530

    Create __init__.py

---
+++ omnichronal_hypersentience_array/infniversal_axiom_resonator/__init__.py
@@ -0,0 +1,3 @@
+# omnichronal_hypersentience_array/infniversal_axiom_resonator/__init__.py
+# Marks the infniversal_axiom_resonator directory as a Python package.
+__all__ = ['InfniversalAxiomResonator']
