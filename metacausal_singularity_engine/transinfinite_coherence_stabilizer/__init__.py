3adabe1e6bd1a0a2d42b071172d2d81b9b5b7120
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:56:26 +0530

    Create __init__.py

---
+++ metacausal_singularity_engine/transinfinite_coherence_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# metacausal_singularity_engine/transinfinite_coherence_stabilizer/__init__.py
+# Marks the transinfinite_coherence_stabilizer directory as a Python package.
+__all__ = ['TransinfiniteCoherenceStabilizer']
