709c10caabe460e4190ecf9fa5db7b9ab106a1d0
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 17:03:12 +0530

    Create __init__.py

---
+++ infiniversal_fractal_synthesis/transmetatemporal_coherence_resonator/__init__.py
@@ -0,0 +1,3 @@
+# infniversal_fractal_synthesis/transmetatemporal_coherence_resonator/__init__.py
+# Marks the transmetatemporal_coherence_resonator directory as a Python package.
+__all__ = ['TransmetatemporalCoherenceResonator']
