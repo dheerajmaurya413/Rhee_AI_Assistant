60123b843e9517fa4d80055c5b095460d8346b49
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 21:42:30 +0530

    Create __init__.py

---
+++ omnichronal_hypersentience_array/metacausal_singularity_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# omnichronal_hypersentience_array/metacausal_singularity_stabilizer/__init__.py
+# Marks the metacausal_singularity_stabilizer directory as a Python package.
+__all__ = ['MetacausalSingularityStabilizer']
