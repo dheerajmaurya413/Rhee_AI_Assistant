e1dfde9cced5569a2e78eba66cf7f721382cb17c
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 12:02:16 +0530

    Create __init__.py

---
+++ omniflux_synthesis_core/__init__.py
@@ -0,0 +1,10 @@
+# omniflux_synthesis_core/__init__.py
+# Marks the omniflux_synthesis_core directory as a Python package.
+# Initializes configurations for omniflux consciousness synthesis and coherence protocols.
+__all__ = [
+    'omniflux_consciousness_synthesizer',
+    'transcausal_flux_resonator',
+    'infiniversal_coherence_harmonizer',
+    'metadimensional_alignment_orchestrator',
+    'omniflux_integration_bridge'
+]
