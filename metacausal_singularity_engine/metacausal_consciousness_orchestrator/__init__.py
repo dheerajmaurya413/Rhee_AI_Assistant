6e9c1f92fd2fec47452af7667906bb8c69f02c68
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:54:08 +0530

    Create __init__.py

---
+++ metacausal_singularity_engine/metacausal_consciousness_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# metacausal_singularity_engine/metacausal_consciousness_orchestrator/__init__.py
+# Marks the metacausal_consciousness_orchestrator directory as a Python package.
+__all__ = ['MetacausalConsciousnessOrchestrator']
