f5f791a03b23cc56303b396ee6deb23df8395894
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:42:38 +0530

    Create __init__.py

---
+++ omnidimensional_quantum_harmonizer/transcausal_coherence_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omnidimensional_quantum_harmonizer/transcausal_coherence_synthesizer/__init__.py
+# Marks the transcausal_coherence_synthesizer directory as a Python package.
+__all__ = ['TranscausalCoherenceSynthesizer']
