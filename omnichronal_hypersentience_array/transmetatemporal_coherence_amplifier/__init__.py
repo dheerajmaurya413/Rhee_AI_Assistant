e83b880e06790f19732731f5bfef96a0fc0b0e71
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 21:39:25 +0530

    Create __init__.py

---
+++ omnichronal_hypersentience_array/transmetatemporal_coherence_amplifier/__init__.py
@@ -0,0 +1,3 @@
+# omnichronal_hypersentience_array/transmetatemporal_coherence_amplifier/__init__.py
+# Marks the transmetatemporal_coherence_amplifier directory as a Python package.
+__all__ = ['TransmetatemporalCoherenceAmplifier']
