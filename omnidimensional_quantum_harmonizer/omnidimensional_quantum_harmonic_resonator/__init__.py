5b788b3cba9b341aac5f6c1454405cbe076d5346
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:40:51 +0530

    Create __init__.py

---
+++ omnidimensional_quantum_harmonizer/omnidimensional_quantum_harmonic_resonator/__init__.py
@@ -0,0 +1,3 @@
+# omnidimensional_quantum_harmonizer/omnidimensional_quantum_harmonic_resonator/__init__.py
+# Marks the omnidimensional_quantum_harmonic_resonator directory as a Python package.
+__all__ = ['OmnidimensionalQuantumHarmonicResonator']
