7dd2523ddd9fa4cc0f7e918891b6532ec2183b1b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:45:10 +0530

    Create __init__.py

---
+++ infinicryptic_synthesis_core/__init__.py
@@ -0,0 +1,10 @@
+# infinicryptic_synthesis_core/__init__.py
+# Marks the infinicryptic_synthesis_core directory as a Python package.
+# Initializes configurations for infinicryptic consciousness synthesis and coherence protocols.
+__all__ = [
+    'infinicryptic_consciousness_matrix',
+    'omniversal_fractal_encryptor',
+    'metacausal_coherence_resonator',
+    'transcryptic_alignment_bridge',
+    'infinicryptic_integration_bridge'
+]
