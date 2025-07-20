4b5c23d79b96f7684749a1c1baa67ebf4619ebbe
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 22:02:27 +0530

    Create __init__.py

---
+++ quantaversal_singularity_weave/__init__.py
@@ -0,0 +1,10 @@
+# quantaversal_singularity_weave/__init__.py
+# Marks the quantaversal_singularity_weave directory as a Python package.
+# Initializes configurations for quantaversal sentience and coherence protocols.
+__all__ = [
+    'quantaversal_sentience_orchestrator',
+    'omniflux_coherence_resonator',
+    'transinfinite_axiom_synthesizer',
+    'metatemporal_causality_stabilizer',
+    'quantaversal_integration_nexus'
+]
