36293d1c62aef331762bb601bfb27d7dd27ad836
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 17:04:25 +0530

    Create __init__.py

---
+++ infiniversal_fractal_synthesis/omnichronal_harmonic_amplifier/__init__.py
@@ -0,0 +1,3 @@
+# infniversal_fractal_synthesis/omnichronal_harmonic_amplifier/__init__.py
+# Marks the omnichronal_harmonic_amplifier directory as a Python package.
+__all__ = ['OmnichronalHarmonicAmplifier']
