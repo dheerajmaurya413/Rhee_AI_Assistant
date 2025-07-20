32e9a8122ea546500dbca06082998f96cd5ac112
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:24:23 +0530

    Create __init__.py

---
+++ transmetagalactic_synthesis_array/transmetagalactic_integration_nexus/__init__.py
@@ -0,0 +1,3 @@
+# transmetagalactic_synthesis_array/transmetagalactic_integration_nexus/__init__.py
+# Marks the transmetagalactic_integration_nexus directory as a Python package.
+__all__ = ['TransmetagalacticIntegrationNexus']
