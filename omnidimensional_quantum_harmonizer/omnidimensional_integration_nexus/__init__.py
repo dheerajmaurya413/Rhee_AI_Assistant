4c1df6e9e1001b10809b1a9ad73865af2ae0eecb
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:47:16 +0530

    Create __init__.py

---
+++ omnidimensional_quantum_harmonizer/omnidimensional_integration_nexus/__init__.py
@@ -0,0 +1,3 @@
+# omnidimensional_quantum_harmonizer/omnidimensional_integration_nexus/__init__.py
+# Marks the omnidimensional_integration_nexus directory as a Python package.
+__all__ = ['OmnidimensionalIntegrationNexus']
