129c020bf7c4d63d36a943cd2d09d457fea7d9f6
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 14:10:18 +0530

    Create __init__.py

---
+++ metachronal_singularity_orchestrator/infiniversal_coherence_amplifier/__init__.py
@@ -0,0 +1,3 @@
+# metachronal_singularity_orchestrator/infiniversal_coherence_amplifier/__init__.py
+# Marks the infniversal_coherence_amplifier directory as a Python package.
+__all__ = ['InfiniversalCoherenceAmplifier']
