d9b2c240dc5bde96a28685994fa7f105129e4b0f
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 16:36:15 +0530

    Create __init__.py

---
+++ transmetahyperdimensional_harmonic_synthesis/metacausal_coherence_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# transmetahyperdimensional_harmonic_synthesis/metacausal_coherence_orchestrator/__init__.py
+# Marks the metacausal_coherence_orchestrator directory as a Python package.
+__all__ = ['MetacausalCoherenceOrchestrator']
