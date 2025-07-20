33dbea15d3ddf7018cc714a58b9449cf93887e4a
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 16:44:09 +0530

    Create __init__.py

---
+++ omnitemporal_quantum_singularity/transcausal_resonance_modulator/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_quantum_singularity/transcausal_resonance_modulator/__init__.py
+# Marks the transcausal_resonance_modulator directory as a Python package.
+__all__ = ['TranscausalResonanceModulator']
