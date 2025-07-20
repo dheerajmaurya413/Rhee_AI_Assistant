2aab57bb573fe4256ebc2b93b0428c81b5fd06f8
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 14:04:30 +0530

    Create __init__.py

---
+++ metachronal_singularity_orchestrator/__init__.py
@@ -0,0 +1,10 @@
+# metachronal_singularity_orchestrator/__init__.py
+# Marks the metachronal_singularity_orchestrator directory as a Python package.
+# Initializes configurations for metachronal singularity and coherence protocols.
+__all__ = [
+    'metachronal_singularity_synthesizer',
+    'infiniversal_coherence_amplifier',
+    'transfractal_resonance_modulator',
+    'omnitemporal_causality_bridge',
+    'metachronal_integration_nexus'
+]
