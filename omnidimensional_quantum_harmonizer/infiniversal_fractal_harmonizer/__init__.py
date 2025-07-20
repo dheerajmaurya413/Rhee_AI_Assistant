0573cf76d6c965ecfcaeb5e5470d15f62bd1d0c0
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:44:26 +0530

    Create __init__.py

---
+++ omnidimensional_quantum_harmonizer/infiniversal_fractal_harmonizer/__init__.py
@@ -0,0 +1,3 @@
+# omnidimensional_quantum_harmonizer/infiniversal_fractal_harmonizer/__init__.py
+# Marks the infniversal_fractal_harmonizer directory as a Python package.
+__all__ = ['InfiniversalFractalHarmonizer']
