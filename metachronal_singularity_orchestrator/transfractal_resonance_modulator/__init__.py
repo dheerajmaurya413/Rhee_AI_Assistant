3cf21e58aad08c8a7b5f32a21656ff01e9e6bcc6
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 14:12:23 +0530

    Create __init__.py

---
+++ metachronal_singularity_orchestrator/transfractal_resonance_modulator/__init__.py
@@ -0,0 +1,3 @@
+# metachronal_singularity_orchestrator/transfractal_resonance_modulator/__init__.py
+# Marks the transfractal_resonance_modulator directory as a Python package.
+__all__ = ['TransfractalResonanceModulator']
