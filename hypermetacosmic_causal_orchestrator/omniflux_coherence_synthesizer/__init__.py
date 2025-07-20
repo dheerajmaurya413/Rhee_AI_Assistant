739539a9f618a8b3d0096a2ec71189d64192f314
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 17:12:49 +0530

    Create __init__.py

---
+++ hypermetacosmic_causal_orchestrator/omniflux_coherence_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# hypermetacosmic_causal_orchestrator/omniflux_coherence_synthesizer/__init__.py
+# Marks the omniflux_coherence_synthesizer directory as a Python package.
+__all__ = ['OmnifluxCoherenceSynthesizer']
