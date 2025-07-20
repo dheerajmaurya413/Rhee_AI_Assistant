ad5e917bb983d85b1c6f922e71e28296ab455ab8
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:55:14 +0530

    Create __init__.py

---
+++ metacausal_singularity_engine/omnichronal_causality_modulator/__init__.py
@@ -0,0 +1,3 @@
+# metacausal_singularity_engine/omnichronal_causality_modulator/__init__.py
+# Marks the omnichronal_causality_modulator directory as a Python package.
+__all__ = ['OmnichronalCausalityModulator']
