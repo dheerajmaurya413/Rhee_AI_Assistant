c52921430f0176d6fe35bed6a846f1f135b5e85b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:58:41 +0530

    Create __init__.py

---
+++ transomniversal_coherence_matrix/infinicryptic_fractal_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# transomniversal_coherence_matrix/infinicryptic_fractal_orchestrator/__init__.py
+# Marks the infinicryptic_fractal_orchestrator directory as a Python package.
+__all__ = ['InfinicrypticFractalOrchestrator']
