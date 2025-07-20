842286bb7cdc82d60cdfba02c121be9f556b0335
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 11:46:55 +0530

    Create __init__.py

---
+++ infinicryptic_synthesis_core/omniversal_fractal_encryptor/__init__.py
@@ -0,0 +1,3 @@
+# infinicryptic_synthesis_core/omniversal_fractal_encryptor/__init__.py
+# Marks the omniversal_fractal_encryptor directory as a Python package.
+__all__ = ['OmniversalFractalEncryptor']
