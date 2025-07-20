799f737f1c6dce414e26d010d3f15968cea2dd68
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 13:06:57 +0530

    Create __init__.py

---
+++ hyperfractal_consciousness_matrix/metatemporal_fractal_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# hyperfractal_consciousness_matrix/metatemporal_fractal_orchestrator/__init__.py
+# Marks the metatemporal_fractal_orchestrator directory as a Python package.
+__all__ = ['MetatemporalFractalOrchestrator']
