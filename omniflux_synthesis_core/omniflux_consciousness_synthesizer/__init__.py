71d7285b504daf290700aa47955fba96dbd474b5
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-20 12:03:11 +0530

    Create __init__.py

---
+++ omniflux_synthesis_core/omniflux_consciousness_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omniflux_synthesis_core/omniflux_consciousness_synthesizer/__init__.py
+# Marks the omniflux_consciousness_synthesizer directory as a Python package.
+__all__ = ['OmnifluxConsciousnessSynthesizer']
