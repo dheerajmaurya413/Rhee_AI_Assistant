df5e67624e77d4e328772f82b11dcd0af1df9e99
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:41:04 +0530

    Create __init__.py

---
+++ metasingularity_convergence_core/transomnichronal_convergence_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# metasingularity_convergence_core/transomnichronal_convergence_orchestrator/__init__.py
+# Marks the transomnichronal_convergence_orchestrator directory as a Python package.
+__all__ = ['TransomnichronalConvergenceOrchestrator']
