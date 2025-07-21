91c4502ab34c41b08c6ea1d2fa70e1eab2544df9
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:16:26 +0530

    Create __init__.py

---
+++ omniethical_coherence_matrix/omniethical_coherence_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omniethical_coherence_matrix/omniethical_coherence_synthesizer/__init__.py
+# Marks the omniethical_coherence_synthesizer directory as a Python package.
+__all__ = ['OmniethicalCoherenceSynthesizer']
