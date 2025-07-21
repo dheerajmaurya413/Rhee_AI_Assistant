8854dadf0710f608ec8688d0f50c89d9220d9472
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:29:09 +0530

    Create __init__.py

---
+++ hyperdimensional_axiom_weaver/metatemporal_axiom_resonator/__init__.py
@@ -0,0 +1,3 @@
+# hyperdimensional_axiom_weaver/metatemporal_axiom_resonator/__init__.py
+# Marks the metatemporal_axiom_resonator directory as a Python package.
+__all__ = ['MetatemporalAxiomResonator']
