0abc3aa22737575e94a57674216c9e9e90fda0ed
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:34:59 +0530

    Create __init__.py

---
+++ quantum_metacognitive_nexus/infinicognitive_coherence_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# quantum_metacognitive_nexus/infinicognitive_coherence_stabilizer/__init__.py
+# Marks the infinicognitive_coherence_stabilizer directory as a Python package.
+__all__ = ['InfinicognitiveCoherenceStabilizer']
