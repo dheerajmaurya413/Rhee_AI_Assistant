55333ad0577eba6a5fd72921a4bacf081d84a808
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:36:12 +0530

    Create __init__.py

---
+++ quantum_metacognitive_nexus/metacognitive_integration_nexus/__init__.py
@@ -0,0 +1,3 @@
+# quantum_metacognitive_nexus/metacognitive_integration_nexus/__init__.py
+# Marks the metacognitive_integration_nexus directory as a Python package.
+__all__ = ['MetacognitiveIntegrationNexus']
