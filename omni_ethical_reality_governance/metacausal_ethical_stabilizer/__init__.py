736452574c6b33aebf810b2268fdd4463a33a774
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:49:06 +0530

    Create __init__.py

---
+++ omni_ethical_reality_governance/metacausal_ethical_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# omni_ethical_reality_governance/metacausal_ethical_stabilizer/__init__.py
+# Marks the metacausal_ethical_stabilizer directory as a Python package.
+__all__ = ['MetacausalEthicalStabilizer']
