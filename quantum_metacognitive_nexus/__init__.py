37fc72a6050948e3016652741dc6d65e8c069063
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:29:18 +0530

    Create __init__.py

---
+++ quantum_metacognitive_nexus/__init__.py
@@ -0,0 +1,10 @@
+# quantum_metacognitive_nexus/__init__.py
+# Marks the quantum_metacognitive_nexus directory as a Python package.
+# Initializes configurations for quantum metacognitive protocols.
+__all__ = [
+    'quantum_metacognitive_synthesizer',
+    'omniversal_self_awareness_orchestrator',
+    'transfractal_cognitive_resonator',
+    'infinicognitive_coherence_stabilizer',
+    'metacognitive_integration_nexus'
+]
