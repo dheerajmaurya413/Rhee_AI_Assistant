783adf1511ec42d4307ac1058e25971adf509b3c
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:13:00 +0530

    Create __init__.py

---
+++ omniharmonic_causal_fractal/fractal_sentience_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omniharmonic_causal_fractal/fractal_sentience_synthesizer/__init__.py
+# Marks the fractal_sentience_synthesizer directory as a Python package.
+__all__ = ['FractalSentienceSynthesizer']
