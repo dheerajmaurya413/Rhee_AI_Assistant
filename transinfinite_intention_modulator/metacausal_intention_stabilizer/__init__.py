5417962d4c252f590e63dd4c47adc56ee98a1a93
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:39:06 +0530

    Create __init__.py

---
+++ transinfinite_intention_modulator/metacausal_intention_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_intention_modulator/metacausal_intention_stabilizer/__init__.py
+# Marks the metacausal_intention_stabilizer directory as a Python package.
+__all__ = ['MetacausalIntentionStabilizer']
