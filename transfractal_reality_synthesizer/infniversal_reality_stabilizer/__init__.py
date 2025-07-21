cc66398287e681d7cc9e34d240155626507791a8
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:07:15 +0530

    Create __init__.py

---
+++ transfractal_reality_synthesizer/infniversal_reality_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# transfractal_reality_synthesizer/infniversal_reality_stabilizer/__init__.py
+# Marks the infniversal_reality_stabilizer directory as a Python package.
+__all__ = ['InfniversalRealityStabilizer']
