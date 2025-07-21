c2cdcc2c051c4227a529d3fd65db2b76c426ead1
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:02:12 +0530

    Create __init__.py

---
+++ transfractal_reality_synthesizer/transfractal_reality_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# transfractal_reality_synthesizer/transfractal_reality_synthesizer/__init__.py
+# Marks the transfractal_reality_synthesizer directory as a Python package.
+__all__ = ['TransfractalRealitySynthesizer']
