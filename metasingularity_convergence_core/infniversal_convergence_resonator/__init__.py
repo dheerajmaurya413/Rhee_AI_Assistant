4b22867359ba0e373b44bd271e39aa5e50e1aeea
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:42:22 +0530

    Create __init__.py

---
+++ metasingularity_convergence_core/infniversal_convergence_resonator/__init__.py
@@ -0,0 +1,3 @@
+# metasingularity_convergence_core/infniversal_convergence_resonator/__init__.py
+# Marks the infniversal_convergence_resonator directory as a Python package.
+__all__ = ['InfniversalConvergenceResonator']
