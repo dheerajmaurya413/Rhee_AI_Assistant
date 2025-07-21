950141e8ae8571e0e7e8c011c477b28a71b4ea58
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:26:36 +0530

    Create __init__.py

---
+++ hyperdimensional_axiom_weaver/hyperdimensional_axiom_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# hyperdimensional_axiom_weaver/hyperdimensional_axiom_synthesizer/__init__.py
+# Marks the hyperdimensional_axiom_synthesizer directory as a Python package.
+__all__ = ['HyperdimensionalAxiomSynthesizer']
