f63a577b3f933e3ba95b449ffe1ecdc01bba5e60
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:44:46 +0530

    Create __init__.py

---
+++ omni_ethical_reality_governance/ethical_framework_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# omni_ethical_reality_governance/ethical_framework_synthesizer/__init__.py
+# Marks the ethical_framework_synthesizer directory as a Python package.
+__all__ = ['EthicalFrameworkSynthesizer']
