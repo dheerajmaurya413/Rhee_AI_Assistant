218a2583d4f045fce9c4f9a2de6bfd4da7774a9d
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:05:32 +0530

    Create __init__.py

---
+++ transfractal_reality_synthesizer/metadimensional_reality_resonator/__init__.py
@@ -0,0 +1,3 @@
+# transfractal_reality_synthesizer/metadimensional_reality_resonator/__init__.py
+# Marks the metadimensional_reality_resonator directory as a Python package.
+__all__ = ['MetadimensionalRealityResonator']
