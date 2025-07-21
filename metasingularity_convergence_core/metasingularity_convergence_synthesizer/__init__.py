7ebd845ab3d3e81030d7f9161c8e7d6a7994d7a4
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:39:42 +0530

    Create __init__.py

---
+++ metasingularity_convergence_core/metasingularity_convergence_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# metasingularity_convergence_core/metasingularity_convergence_synthesizer/__init__.py
+# Marks the metasingularity_convergence_synthesizer directory as a Python package.
+__all__ = ['MetasingularityConvergenceSynthesizer']
