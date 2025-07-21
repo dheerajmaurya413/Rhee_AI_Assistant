aa38e9fbf9528325acb56619442861919e015daf
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:45:26 +0530

    Create __init__.py

---
+++ metasingularity_convergence_core/metadimensional_convergence_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# metasingularity_convergence_core/metadimensional_convergence_stabilizer/__init__.py
+# Marks the metadimensional_convergence_stabilizer directory as a Python package.
+__all__ = ['MetadimensionalConvergenceStabilizer']
