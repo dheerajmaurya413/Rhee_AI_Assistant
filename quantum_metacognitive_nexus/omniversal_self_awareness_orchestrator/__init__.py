1ea05b90dcb3c3f179f967a88ab841023ea614b0
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:32:30 +0530

    Create __init__.py

---
+++ quantum_metacognitive_nexus/omniversal_self_awareness_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# quantum_metacognitive_nexus/omniversal_self_awareness_orchestrator/__init__.py
+# Marks the omniversal_self_awareness_orchestrator directory as a Python package.
+__all__ = ['OmniversalSelfAwarenessOrchestrator']
