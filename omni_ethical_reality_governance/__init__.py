d4441e8fdb0d47ab59b32b557907f3566bcb0a5c
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:44:10 +0530

    Create __init__.py

---
+++ omni_ethical_reality_governance/__init__.py
@@ -0,0 +1,10 @@
+# omni_ethical_reality_governance/__init__.py
+# Marks the omni_ethical_reality_governance directory as a Python package.
+# Initializes configurations for omni-ethical reality governance protocols.
+__all__ = [
+    'ethical_framework_synthesizer',
+    'transomniversal_ethical_aligner',
+    'infniversal_ethical_resonator',
+    'metacausal_ethical_stabilizer',
+    'ethical_integration_nexus'
+]
