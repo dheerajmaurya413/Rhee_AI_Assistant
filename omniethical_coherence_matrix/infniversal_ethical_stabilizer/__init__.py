84355509d27fd27db2a83f5c3c6b79aca2e40ba8
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:20:23 +0530

    Create __init__.py

---
+++ omniethical_coherence_matrix/infniversal_ethical_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# omniethical_coherence_matrix/infniversal_ethical_stabilizer/__init__.py
+# Marks the infniversal_ethical_stabilizer directory as a Python package.
+__all__ = ['InfniversalEthicalStabilizer']
