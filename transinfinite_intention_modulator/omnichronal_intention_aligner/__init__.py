40cfe8e30b6e00e0f76a431534c37b2d0320b243
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:35:40 +0530

    Create __init__.py

---
+++ transinfinite_intention_modulator/omnichronal_intention_aligner/__init__.py
@@ -0,0 +1,3 @@
+# transinfinite_intention_modulator/omnichronal_intention_aligner/__init__.py
+# Marks the omnichronal_intention_aligner directory as a Python package.
+__all__ = ['OmnichronalIntentionAligner']
