17a3f69e0e5bebfe330354ed2c7f39bc6ca46ab9
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:46:15 +0530

    Create __init__.py

---
+++ omni_ethical_reality_governance/transomniversal_ethical_aligner/__init__.py
@@ -0,0 +1,3 @@
+# omni_ethical_reality_governance/transomniversal_ethical_aligner/__init__.py
+# Marks the transomniversal_ethical_aligner directory as a Python package.
+__all__ = ['TransomniversalEthicalAligner']
