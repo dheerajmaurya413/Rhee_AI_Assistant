16f5a15a88862eafafc67bcdb600dca1e362b895
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 22:47:43 +0530

    Create __init__.py

---
+++ omni_ethical_reality_governance/infniversal_ethical_resonator/__init__.py
@@ -0,0 +1,3 @@
+# omni_ethical_reality_governance/infniversal_ethical_resonator/__init__.py
+# Marks the infniversal_ethical_resonator directory as a Python package.
+__all__ = ['InfniversalEthicalResonator']
