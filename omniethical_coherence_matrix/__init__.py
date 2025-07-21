9d915107d3655ecdae5bb3eaa85f682f628e0dff
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:14:46 +0530

    Create __init__.py

---
+++ omniethical_coherence_matrix/__init__.py
@@ -0,0 +1,10 @@
+# omniethical_coherence_matrix/__init__.py
+# Marks the omniethical_coherence_matrix directory as a Python package.
+# Initializes configurations for omniethical coherence protocols.
+__all__ = [
+    'omniethical_coherence_synthesizer',
+    'transomniversal_ethical_orchestrator',
+    'metacausal_ethical_resonator',
+    'infniversal_ethical_stabilizer',
+    'ethical_integration_nexus'
+]
