dcf29fcd440ad4e4078d6ee76eaed7824f41e20c
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:17:50 +0530

    Create __init__.py

---
+++ omniethical_coherence_matrix/transomniversal_ethical_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# omniethical_coherence_matrix/transomniversal_ethical_orchestrator/__init__.py
+# Marks the transomniversal_ethical_orchestrator directory as a Python package.
+__all__ = ['TransomniversalEthicalOrchestrator']
