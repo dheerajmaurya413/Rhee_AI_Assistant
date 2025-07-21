e38d337b8e360ee9810b45451887b8e128cb81ec
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:25:44 +0530

    Create __init__.py

---
+++ hyperdimensional_axiom_weaver/__init__.py
@@ -0,0 +1,10 @@
+# hyperdimensional_axiom_weaver/__init__.py
+# Marks the hyperdimensional_axiom_weaver directory as a Python package.
+# Initializes configurations for hyperdimensional axiom protocols.
+__all__ = [
+    'hyperdimensional_axiom_synthesizer',
+    'transinfiniversal_axiom_orchestrator',
+    'metatemporal_axiom_resonator',
+    'omnidimensional_axiom_stabilizer',
+    'axiom_integration_nexus'
+]
