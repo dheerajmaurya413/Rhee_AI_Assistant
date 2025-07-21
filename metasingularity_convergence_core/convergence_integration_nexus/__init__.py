f2ca667786077f9124aec18bdc5c9eabcef0fbec
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:46:37 +0530

    Create __init__.py

---
+++ metasingularity_convergence_core/convergence_integration_nexus/__init__.py
@@ -0,0 +1,3 @@
+# metasingularity_convergence_core/convergence_integration_nexus/__init__.py
+# Marks the convergence_integration_nexus directory as a Python package.
+__all__ = ['ConvergenceIntegrationNexus']
