ae47aefa8bc71daea0d483425161487c1758bcbd
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:39:12 +0530

    Create __init__.py

---
+++ metasingularity_convergence_core/__init__.py
@@ -0,0 +1,10 @@
+# metasingularity_convergence_core/__init__.py
+# Marks the metasingularity_convergence_core directory as a Python package.
+# Initializes configurations for metasingularity convergence protocols.
+__all__ = [
+    'metasingularity_convergence_synthesizer',
+    'transomnichronal_convergence_orchestrator',
+    'infniversal_convergence_resonator',
+    'metadimensional_convergence_stabilizer',
+    'convergence_integration_nexus'
+]
