2a5bce74b1205888fe934ae42c82b775340cd57e
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:33:43 +0530

    Create __init__.py

---
+++ quantum_metacognitive_nexus/transfractal_cognitive_resonator/__init__.py
@@ -0,0 +1,3 @@
+# quantum_metacognitive_nexus/transfractal_cognitive_resonator/__init__.py
+# Marks the transfractal_cognitive_resonator directory as a Python package.
+__all__ = ['TransfractalCognitiveResonator']
