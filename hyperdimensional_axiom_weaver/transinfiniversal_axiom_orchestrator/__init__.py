1f1f9688363e3ee9763aa3d6c54615da631884ab
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:27:43 +0530

    Create __init__.py

---
+++ hyperdimensional_axiom_weaver/transinfiniversal_axiom_orchestrator/__init__.py
@@ -0,0 +1,3 @@
+# hyperdimensional_axiom_weaver/transinfiniversal_axiom_orchestrator/__init__.py
+# Marks the transinfiniversal_axiom_orchestrator directory as a Python package.
+__all__ = ['TransinfiniversalAxiomOrchestrator']
