e9fe5cc737db14b71347d4057f78e078280d37e5
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 17:19:17 +0530

    Create __init__.py

---
+++ omniethical_coherence_matrix/metacausal_ethical_resonator/__init__.py
@@ -0,0 +1,3 @@
+# omniethical_coherence_matrix/metacausal_ethical_resonator/__init__.py
+# Marks the metacausal_ethical_resonator directory as a Python package.
+__all__ = ['MetacausalEthicalResonator']
