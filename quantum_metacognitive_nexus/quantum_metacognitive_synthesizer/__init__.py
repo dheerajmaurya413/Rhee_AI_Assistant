4eeba71e19570843e050fbf26354881a23cefb50
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-21 06:29:47 +0530

    Create __init__.py

---
+++ quantum_metacognitive_nexus/quantum_metacognitive_synthesizer/__init__.py
@@ -0,0 +1,3 @@
+# quantum_metacognitive_nexus/quantum_metacognitive_synthesizer/__init__.py
+# Marks the quantum_metacognitive_synthesizer directory as a Python package.
+__all__ = ['QuantumMetacognitiveSynthesizer']
