b1fe49437e14af8a858e7a62305db93ed0773440
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:48:31 +0530

    Create __init__.py

---
+++ omnitemporal_coherence_lattice/infniversal_temporal_resonator/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_coherence_lattice/infniversal_temporal_resonator/__init__.py
+# Marks the infniversal_temporal_resonator directory as a Python package.
+__all__ = ['InfniversalTemporalResonator']
