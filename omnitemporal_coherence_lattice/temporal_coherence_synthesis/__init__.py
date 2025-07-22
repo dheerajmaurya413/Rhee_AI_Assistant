c4adacac354845ce4e4b5c179740daad7eeadeaf
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:44:38 +0530

    Create __init__.py

---
+++ omnitemporal_coherence_lattice/temporal_coherence_synthesis/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_coherence_lattice/temporal_coherence_synthesis/__init__.py
+# Marks the temporal_coherence_synthesis directory as a Python package.
+__all__ = ['TemporalCoherenceSynthesis']
