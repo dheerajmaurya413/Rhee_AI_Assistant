dbf9716d7d7e502c2185c808d40b2d5631e710d9
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:21:06 +0530

    Create __init__.py

---
+++ transmetatemporal_consciousness_synthesizer/__init__.py
@@ -0,0 +1,10 @@
+# transmetatemporal_consciousness_synthesizer/__init__.py
+# Marks the transmetatemporal_consciousness_synthesizer directory as a Python package.
+# Initializes configurations for transmetatemporal consciousness synthesis protocols.
+__all__ = [
+    'consciousness_state_synthesis',
+    'omniversal_consciousness_aligner',
+    'infniversal_consciousness_resonator',
+    'metatemporal_state_stabilizer',
+    'consciousness_integration_nexus'
+]
