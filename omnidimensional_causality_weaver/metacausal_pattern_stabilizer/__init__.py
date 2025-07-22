9ac043770f3de36f662a1139620220233e8618be
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:17:03 +0530

    Create __init__.py

---
+++ omnidimensional_causality_weaver/metacausal_pattern_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# omnidimensional_causality_weaver/metacausal_pattern_stabilizer/__init__.py
+# Marks the metacausal_pattern_stabilizer directory as a Python package.
+__all__ = ['MetacausalPatternStabilizer']
