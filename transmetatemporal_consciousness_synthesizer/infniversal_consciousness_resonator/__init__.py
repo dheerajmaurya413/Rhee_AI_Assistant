7c32144afd0914761dc9c68528c624cfb816dca6
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:25:55 +0530

    Create __init__.py

---
+++ transmetatemporal_consciousness_synthesizer/infniversal_consciousness_resonator/__init__.py
@@ -0,0 +1,3 @@
+# transmetatemporal_consciousness_synthesizer/infniversal_consciousness_resonator/__init__.py
+# Marks the infniversal_consciousness_resonator directory as a Python package.
+__all__ = ['InfniversalConsciousnessResonator']
