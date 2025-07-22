b5ca6b6c97236fa064c21dc0984a82c61aeb2a85
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:24:33 +0530

    Create __init__.py

---
+++ transmetatemporal_consciousness_synthesizer/omniversal_consciousness_aligner/__init__.py
@@ -0,0 +1,3 @@
+# transmetatemporal_consciousness_synthesizer/omniversal_consciousness_aligner/__init__.py
+# Marks the omniversal_consciousness_aligner directory as a Python package.
+__all__ = ['OmniversalConsciousnessAligner']
