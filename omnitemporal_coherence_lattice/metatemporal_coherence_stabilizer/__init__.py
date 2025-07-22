4394e51cceaf459aa262c55599131d0b76be0803
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:51:13 +0530

    Create __init__.py

---
+++ omnitemporal_coherence_lattice/metatemporal_coherence_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# omnitemporal_coherence_lattice/metatemporal_coherence_stabilizer/__init__.py
+# Marks the metatemporal_coherence_stabilizer directory as a Python package.
+__all__ = ['MetatemporalCoherenceStabilizer']
