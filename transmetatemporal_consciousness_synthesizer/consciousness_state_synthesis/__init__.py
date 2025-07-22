3878915755337df144b644a5d2e7c39f2bc58902
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:22:11 +0530

    Create __init__.py

---
+++ transmetatemporal_consciousness_synthesizer/consciousness_state_synthesis/__init__.py
@@ -0,0 +1,3 @@
+# transmetatemporal_consciousness_synthesizer/consciousness_state_synthesis/__init__.py
+# Marks the consciousness_state_synthesis directory as a Python package.
+__all__ = ['ConsciousnessStateSynthesis']
