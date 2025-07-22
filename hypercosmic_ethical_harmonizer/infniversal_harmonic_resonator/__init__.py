70578af831f4d7c5a100e6193c597c123668a15b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 07:35:32 +0530

    Create __init__.py

---
+++ hypercosmic_ethical_harmonizer/infniversal_harmonic_resonator/__init__.py
@@ -0,0 +1,3 @@
+# hypercosmic_ethical_harmonizer/infniversal_harmonic_resonator/__init__.py
+# Marks the infniversal_harmonic_resonator directory as a Python package.
+__all__ = ['InfniversalHarmonicResonator']
