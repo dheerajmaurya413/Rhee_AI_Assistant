a0dc4fbf9720e3754cc5b3ac7616db4e1d8cab1b
Dheerej Maurya <73028214+dheerajmaurya413@users.noreply.github.com>
2025-07-22 17:27:20 +0530

    Create __init__.py

---
+++ transmetatemporal_consciousness_synthesizer/metatemporal_state_stabilizer/__init__.py
@@ -0,0 +1,3 @@
+# transmetatemporal_consciousness_synthesizer/metatemporal_state_stabilizer/__init__.py
+# Marks the metatemporal_state_stabilizer directory as a Python package.
+__all__ = ['MetatemporalStateStabilizer']
